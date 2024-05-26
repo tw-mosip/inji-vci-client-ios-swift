965e659a04e80af16bab366e2acbc6bcade895b9
PoojaBabusing <115976560+PoojaBabusing@users.noreply.github.com>
2024-05-26 13:23:24 +0530

    [injimob-1313] refactor vc struct to match the esignet vc structure (#8)
    Signed-off-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
    Co-authored-by: Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>

--- Sources/VCIClient/credentialResponse/types/LdpVcCredentialResponse.swift
+++ Sources/VCIClient/credentialResponse/types/LdpVcCredentialResponse.swift
@@ -1,37 +1,8 @@
 import Foundation
 
-struct VC: Codable{
+struct VC: Codable, CredentialResponse{
     let format: String
     let credential: Credential
-}
-
-struct Credential: Codable , CredentialResponse{
-    let issuer: String
-    let issuanceDate: String
-    let id: String
-    let proof: ProofResponse
-    let credentialSubject: CredentialSubject
-    let type: [String]
-    let contextArray: [String]?
-    let contextDict: [String: String]?
-    
-    enum CodingKeys: String, CodingKey {
-        case credentialSubject, id, proof, type, issuer, issuanceDate
-        case contextArray = "@context"
-        case contextDict
-    }
-    
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        issuer = try values.decode(String.self, forKey: .issuer)
-        issuanceDate = try values.decode(String.self, forKey: .issuanceDate)
-        credentialSubject = try values.decode(CredentialSubject.self, forKey: .credentialSubject)
-        id = try values.decode(String.self, forKey: .id)
-        proof = try values.decode(ProofResponse.self, forKey: .proof)
-        type = try values.decode([String].self, forKey: .type)
-        contextArray = try? values.decode([String].self, forKey: .contextArray)
-        contextDict = try? values.decode([String: String].self, forKey: .contextArray)
-    }
     
     func toJSONString() throws -> String {
         let encoder = JSONEncoder()
@@ -44,6 +15,80 @@ struct Credential: Codable , CredentialResponse{
     }
 }
 
+
+struct Credential: Codable , CredentialResponse{
+    let issuanceDate: String
+    let credentialSubject: CredentialSubject
+    let id: String
+    let proof: ProofResponse
+    let type: [String]
+    let context: [ContextType]?
+    let issuer: String
+    
+    enum CodingKeys: String, CodingKey {
+        case issuanceDate, credentialSubject, id, proof, type
+        case context = "@context"
+        case issuer
+    }
+    
+    func toJSONString() throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let data = try encoder.encode(self)
+        guard let jsonString = String(data: data, encoding: .utf8) else {
+        throw DownloadFailedError.encodingResponseFailed
+        }
+    return jsonString
+    }
+    
+    init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            issuanceDate = try container.decode(String.self, forKey: .issuanceDate)
+            credentialSubject = try container.decode(CredentialSubject.self, forKey: .credentialSubject)
+            id = try container.decode(String.self, forKey: .id)
+            proof = try container.decode(ProofResponse.self, forKey: .proof)
+            type = try container.decode([String].self, forKey: .type)
+            context = try container.decodeIfPresent([ContextType].self, forKey: .context)
+            issuer = try container.decode(String.self, forKey: .issuer)
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(issuanceDate, forKey: .issuanceDate)
+            try container.encode(credentialSubject, forKey: .credentialSubject)
+            try container.encode(id, forKey: .id)
+            try container.encode(proof, forKey: .proof)
+            try container.encode(type, forKey: .type)
+            try container.encode(context, forKey: .context)
+            try container.encode(issuer, forKey: .issuer)
+        }
+}
+
+enum ContextType: Codable {
+    case string(String)
+    case dictionary([String: String])
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let string = try? container.decode(String.self) {
+            self = .string(string)
+        } else if let dictionary = try? container.decode([String: String].self) {
+            self = .dictionary(dictionary)
+        } else {
+            throw DownloadFailedError.encodingResponseFailed
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+            case .string(let value):
+                try container.encode(value)
+            case .dictionary(let value):
+                try container.encode(value)
+        }
+    }
+}
 
 struct CredentialSubject: Codable {
     let gender: [Gender]
--- Sources/VCIClient/credentialResponse/types/LdpVcCredentialResponseFactory.swift
+++ Sources/VCIClient/credentialResponse/types/LdpVcCredentialResponseFactory.swift
@@ -4,7 +4,7 @@ class LdpVcCredentialResponseFactory: CredentialResponseFactoryProtocol {
     func constructResponse(response: Data) throws -> CredentialResponse? {
         do{
             let vc = try JSONDecoder().decode(VC.self, from: response)
-            return vc.credential
+            return vc
         }
         catch{
             throw DownloadFailedError.decodingResponseFailed
